9fe39c7d0f11242fd35010940a73012ed590b50f
noob-programmer9012 <parth.sclub@gmail.com>
2024-09-24 00:25:13 +0530

    added btop and mongosh to toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,9 +6,19 @@ return {
   config = function()
     local Terminal = require("toggleterm.terminal").Terminal
     local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "tab" })
+    local btop = Terminal:new({ cmd = "btop", hidden = true, direction = "tab" })
+    local mongosh = Terminal:new({ cmd = "mongosh", hidden = true, direction = "tab" })
 
     function Lazygit_toggle()
       lazygit:toggle()
+    end
+
+    function Btop_toggle()
+      btop:toggle()
+    end
+
+    function Mongosh_toggle()
+      mongosh:toggle()
     end
 
     require("toggleterm").setup({
@@ -23,5 +33,7 @@ return {
     })
 
     vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>lua Btop_toggle()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>gm", "<cmd>lua Mongosh_toggle()<CR>", { noremap = true, silent = true })
   end,
 }
