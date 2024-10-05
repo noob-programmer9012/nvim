fd16dfc89a54f64a4b082ddf023d8193dd6a827b
noob-programmer9012 <parth.sclub@gmail.com>
2024-10-05 12:35:42 +0530

    run ollama from toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,39 +1,56 @@
 return {
 
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {},
-  config = function()
-    local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "tab" })
-    local btop = Terminal:new({ cmd = "btop", hidden = true, direction = "tab" })
-    local mongosh = Terminal:new({ cmd = "mongosh", hidden = true, direction = "tab" })
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {},
+	config = function()
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "tab" })
+		local btop = Terminal:new({ cmd = "btop", hidden = true, direction = "tab" })
+		local mongosh = Terminal:new({ cmd = "mongosh", hidden = true, direction = "tab" })
+		local ollama = Terminal:new({ cmd = "ollama run llama3.2", hidden = true, direction = "tab" })
+		local ollamastop = Terminal:new({ cmd = "sudo pkill ollama_llama_se", hidden = true, direction = "tab" })
 
-    function Lazygit_toggle()
-      lazygit:toggle()
-    end
+		function Lazygit_toggle()
+			lazygit:toggle()
+		end
 
-    function Btop_toggle()
-      btop:toggle()
-    end
+		function Btop_toggle()
+			btop:toggle()
+		end
 
-    function Mongosh_toggle()
-      mongosh:toggle()
-    end
+		function Mongosh_toggle()
+			mongosh:toggle()
+		end
 
-    require("toggleterm").setup({
-      size = 30,
-      open_mapping = [[<c-\>]],
-      direction = "tab",
-      float_opts = {
-        border = "curved",
-        width = 130,
-        height = 35,
-      },
-    })
+		function Ollama_toggle()
+			ollama:toggle()
+		end
 
-    vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>lua Btop_toggle()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>gm", "<cmd>lua Mongosh_toggle()<CR>", { noremap = true, silent = true })
-  end,
+		function OllamaStop_toggle()
+			ollamastop:toggle()
+		end
+
+		require("toggleterm").setup({
+			size = 30,
+			open_mapping = [[<c-\>]],
+			direction = "tab",
+			float_opts = {
+				border = "curved",
+				width = 130,
+				height = 35,
+			},
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>lua Btop_toggle()<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader>gm", "<cmd>lua Mongosh_toggle()<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader>go", "<cmd>lua Ollama_toggle()<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>gs",
+			"<cmd>lua OllamaStop_toggle()<CR>parthpatel@9012<CR>",
+			{ noremap = true, silent = true }
+		)
+	end,
 }
