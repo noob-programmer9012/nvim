fb8632ddab50458d63389e633d5a31e207fa81a8
noob-programmer9012 <parth.sclub@gmail.com>
2024-12-25 19:27:31 +0530

    change in pretty-fold file

--- lua/plugins/pretty-fold.lua
+++ lua/plugins/pretty-fold.lua
@@ -1,17 +1,17 @@
 return {
-	{
-		"anuvyklack/pretty-fold.nvim",
-		config = function()
-			require("pretty-fold").setup()
-		end,
-	},
-	{
-		"anuvyklack/fold-preview.nvim",
-		requires = "anuvyklack/keymap-amend.nvim",
-		config = function()
-			require("fold-preview").setup({
-				-- Your configuration goes here.
-			})
-		end,
-	},
+  {
+    "anuvyklack/pretty-fold.nvim",
+    config = function()
+      require("pretty-fold").setup()
+    end,
+  },
+  -- {
+  -- 	"anuvyklack/fold-preview.nvim",
+  -- 	requires = "anuvyklack/keymap-amend.nvim",
+  -- 	config = function()
+  -- 		require("fold-preview").setup({
+  -- 			-- Your configuration goes here.
+  -- 		})
+  -- 	end,
+  -- },
 }
